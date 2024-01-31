3148a330f9f57e8def74b34bbd310c846a9c1d5b
WyattTheWorldly <104187301+WyattTheWorldly@users.noreply.github.com>
2024-01-30 22:17:09 -0800

    Revert "Created flask app"
    This reverts commit 1ae17c2e494e10d4e373832585def96fc5bc9652.

--- Learnix/config.py
+++
@@ -1,4 +0,0 @@
-#config.py
-
-class Config:
-    DB_CONNECTION_STRING="mysql+pymysql://x2ia8i9excbhql3rbwl9:pscale_pw_MHxUizMIvgjH2nGnuUfmkpzPsy7SR94IohOKcSZAcbR@aws.connect.psdb.cloud/learnixdatabase?charset=utf8mb4"
