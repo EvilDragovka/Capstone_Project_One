1ae17c2e494e10d4e373832585def96fc5bc9652
WyattTheWorldly <104187301+WyattTheWorldly@users.noreply.github.com>
2024-01-30 22:15:41 -0800

    Created flask app
    Flask app has been created and is linked to a Cloud DB.

---
+++ Learnix/config.py
@@ -0,0 +1,4 @@
+#config.py
+
+class Config:
+    DB_CONNECTION_STRING="mysql+pymysql://x2ia8i9excbhql3rbwl9:pscale_pw_MHxUizMIvgjH2nGnuUfmkpzPsy7SR94IohOKcSZAcbR@aws.connect.psdb.cloud/learnixdatabase?charset=utf8mb4"
