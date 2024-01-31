38dda42cf608422236ad94236755e6c6f91ae570
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2024-01-30 23:10:30 -0800

    everything

---
+++ server/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# /api/home
+@app.route('/api/home', methods=['GET'])
+def return_home():
+    return jsonify({
+        'message': 'Hello, world!'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
