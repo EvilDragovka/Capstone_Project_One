daa47bef0bba6e075644041b23f0ab26e48af489
Bayasgalan <cetrion8@gmail.con>
2024-02-05 00:00:24 -0800

    created the Main Page

---
+++ client/pages/mainPage.tsx
@@ -0,0 +1,15 @@
+// MainPage.jsx
+
+import React, { useState } from 'react';
+
+function MainPage() {
+    // Your component logic goes here
+
+    return (
+        <div>
+            <p>Main Page</p>
+        </div>
+    );
+}
+
+export default MainPage;
