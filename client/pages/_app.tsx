241f81e845565efe1c2f9fc209405152593c026f
Bayasgalan <cetrion8@gmail.con>
2024-02-04 21:16:21 -0800

    imported welcomePage

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import WelcomePage from './welcomePage';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <WelcomePage />
+        <Component {...pageProps} />
+      </>
+  );
 }
