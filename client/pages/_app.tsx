12598aafdc86dd9dd017859a5f4c6ecb6c7d6872
Bayasgalan <cetrion8@gmail.con>
2024-02-04 23:18:56 -0800

    updated the file so I can finnaly navigate through pages -pain-

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import WelcomePage from './welcomePage';
+import SignUpPage from './signUpPage';
+import { useRouter } from 'next/router';
+import MainPage from './mainPage';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <>
-        <WelcomePage />
-        <Component {...pageProps} />
-      </>
-  );
+    const router = useRouter();
+
+    // check the route and render the appropriate component
+    if (router.pathname === '/signUpPage') {
+        return <SignUpPage />;
+    }
+    if (router.pathname === '/mainPage') {
+        return <MainPage />;
+    }
+
+    return <WelcomePage />;
 }
