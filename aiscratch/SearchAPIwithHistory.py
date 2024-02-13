a23f4531a54b65ec416a0834e1f1a067a425dbb5
Zac-dot <xanderthomp@gmail.com>
2024-02-13 15:36:12 -0800

    Made prompt more specific, AI better reacts to general questions

--- aiscratch/SearchAPIwithHistory.py
+++ aiscratch/SearchAPIwithHistory.py
@@ -5,13 +5,15 @@ from langchain.memory import ConversationBufferWindowMemory
 from langchain_community.chat_models.azureml_endpoint import (AzureMLEndpointApiType,LlamaChatContentFormatter,AzureMLChatOnlineEndpoint)
 from langchain.agents import AgentExecutor, LLMSingleActionAgent
 import os
+
+from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
 from CPTemplate import CustomPromptTemplate, CustomOutputParser
 from langchain.tools import DuckDuckGoSearchRun
 from langsmith import Client
 
 # Parses keys for APIs
-# Created with Github Copilot
+# Created with GitHub Copilot
 def get_keys(filename, name):
     with open(filename, 'r') as file:
         for line in file:
@@ -35,31 +37,20 @@ azureKey = get_keys('key.txt', 'AZURE_API_KEY')
 
 url = 'https://Llama2-70bchat-cscapstone-serverless.eastus2.inference.ai.azure.com/v1/chat/completions'
 
-# LLM Model declaration
-llama = AzureMLChatOnlineEndpoint(
-    endpoint_url=url,
-    endpoint_api_type=AzureMLEndpointApiType.serverless,
-    endpoint_api_key=azureKey,
-    content_formatter=LlamaChatContentFormatter(),
-    model_kwargs={"temperature": 0.8, "max_tokens": 1024},
-    streaming=True
-)
-
-# Tool Chain
-search = DuckDuckGoSearchRun()
-tools = [
-    Tool(
-        name="DuckDuckGo",
-        func=search.run,
-        description="Use the tool to search the internet for up-to-date information."
-    )
-]
-
 # Set up a prompt template which can interpolate the history
 # Comes from OpenAI Cookbook
-template_with_history = """You are Learnix, with the main goal to help people with their academics and research. 
-Use tools when necessary, when you know the awnser make sure to state it as the final answer, with Thought
-being "I now know the final answer".
+template_with_history = """You are called Learnix, with the main goal to help people with their academics and research.
+Use tools when necessary, specifically when:
+- The question asks for real-time or current information.
+- The question is highly specific and requires external data to answer accurately.
+- Does not ask for personal information or opinions.
+- Does not ask about "you","your", "Learnix", or "I", references you as an AI, or uses a greeting.
+
+If: - The question is general or not specific - Asks about "you","your", "Learnix", or "I", references you as an AI, 
+or uses a greeting. - Asks for personal information or opinions. - Ask for a joke, riddle, or other non-academic 
+content. You should answer the question directly, meaning you should not use tools and instead should set Thought: I 
+know the answer and Final Answer: the answer to the original input question.
+
 
 You have access to the following tools:
 
@@ -84,15 +75,40 @@ Previous conversation history:
 New question: {input}
 {agent_scratchpad}"""
 
+# LLM Model declaration
+llama = AzureMLChatOnlineEndpoint(
+    endpoint_url=url,
+    endpoint_api_type=AzureMLEndpointApiType.serverless,
+    endpoint_api_key=azureKey,
+    content_formatter=LlamaChatContentFormatter(),
+    model_kwargs={"temperature": 0.8, "max_tokens": 1024},
+    streaming=True
+)
+
+# Tool Chain
+search = DuckDuckGoSearchRun()
+tools = [
+    Tool(
+        name="DuckDuckGo",
+        func=search.run,
+        description="Use the tool to search the internet for up-to-date information."
+    ),
+]
+
+# Custom Prompt Template
 prompt_with_history = CustomPromptTemplate(
     template=template_with_history,
     tools=tools,
     input_variables=["input","intermediate_steps","history"]
 )
 
+# Memory for the chat + LLMChain
+memory = ConversationBufferWindowMemory(k=2, return_messages=True)
 Cake = LLMChain(llm=llama, prompt=prompt_with_history)
 tool_names = [tool.name for tool in tools]
 output_parser = CustomOutputParser()
+
+# The main AI that is being used, and will execute tools
 agent = LLMSingleActionAgent(
     llm_chain=Cake,
     output_parser=output_parser,
@@ -100,14 +116,12 @@ agent = LLMSingleActionAgent(
     allowed_tools=tool_names
 )
 
-memory = ConversationBufferWindowMemory(k=2, return_messages=True)
 agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
 
 print("""
-    Note: Do NOT ask questions that cannot be answered by the tools provided. (Ex: How are you?)
-    Answers will only be provided if they can be factually proven
+    Note: The AI is prone to getting stuck in loops, especially with more open-ended questions.
     Hit break or ctrl+c to stop the program in the event the verbose output is repeating too often
-    This will be fixed in a different iteration with a better event Chain in place to stop this""")
+    This is being looked at and will be fixed in a future iteration""")
 
 # Wait for the user to read the note
 sleep(3)
