e40fd21921e4cc8c095cb1367cca2b8c0e7ce519
Zac-dot <xanderthomp@gmail.com>
2024-02-28 23:25:30 -0800

    Overhauled prompts, adjusted requirements.txt to account for Wikipedia

--- aiscratch/LlamaCompleteTesting.py
+++ aiscratch/LlamaCompleteTesting.py
@@ -1,24 +1,53 @@
 from langchain.memory import ConversationBufferWindowMemory
 
 from server.models.llama_complete import llama_complete
+import warnings
+# Ignore warnings, mainly telling you how to use the APIs
+warnings.simplefilter(action='ignore', category=Warning)
 
-# Testing out llama_complete
+# Adds colors to mark questions asked versus answers given
+# https://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Testing out llama_complete, consists of 10 things
 # Update: this has to be initialized outside the llm, welp
 # But it gives insight on how this exactly works
 router_memory = ConversationBufferWindowMemory(k=5, memory_key="test", return_messages=True)
 
+# Average Latency: 12.56s, at max 26s, absolute limit is 60s
+# Typially 8 out of 10 are answered, 2 are not / get stuck in a slight
+# loop as when using search it does not always parse information
+
 # Testing out geography information
-# Awnsers via llama model
+print(f"{bcolors.OKGREEN}Prompt: What is the captial of France?{bcolors.ENDC}")
 print(llama_complete("What is the capital of France?", router_memory))
 # Testing memory
-# Awnsers via llama model
+print(f"{bcolors.OKGREEN}Prompt: What did I previously ask you?{bcolors.ENDC}")
 print(llama_complete("What did I previously ask you?", router_memory))
 # Testing out summarization of a topic
-# Just awnsers via llama model
+print(f"{bcolors.OKGREEN}Prompt: What papers have Florent Renaud published on arxiv?{bcolors.ENDC}")
 print(llama_complete("What papers have Florent Renaud published on arxiv?", router_memory))
 # Testing out searching of a academic topic on arxiv
-# Uses ARXIV, but fails to summarize
+print(f"{bcolors.OKGREEN}Prompt: What does the paper 1605.08386?{bcolors.ENDC}")
 print(llama_complete("What does the paper 1605.08386 say?", router_memory))
 # Testing out searching recent information
-# Awnsers it via DDG, 50/50 on awnsering it
+print(f"{bcolors.OKGREEN}Prompt: Who won the most recent super bowl?{bcolors.ENDC}")
 print(llama_complete("Who won the most recent super bowl?", router_memory))
+# Testing out recent information
+print(f"{bcolors.OKGREEN}Prompt: When was Animal Crossing New Horizons released?{bcolors.ENDC}")
+print(llama_complete("When was Animal Crossing New Horizons released?", router_memory))
+# Testing out summarization of a topic
+print(f"{bcolors.OKGREEN}Prompt: What is the history of the United States?{bcolors.ENDC}")
+print(llama_complete("What is the history of the United States?", router_memory))
+# Testing out memory
+print(f"{bcolors.OKGREEN}Prompt: What were the last two things i asked you?{bcolors.ENDC}")
+print(llama_complete("What were the last two things i asked you?", router_memory))
--- server/models/llama_complete.py
+++ server/models/llama_complete.py
@@ -1,11 +1,15 @@
 import os
 from datetime import date
+import warnings
+# Ignore warnings, mainly telling you how to use the APIs
+warnings.simplefilter(action='ignore', category=Warning)
 
-from langchain import hub
 from langchain.agents import load_tools, create_react_agent, AgentExecutor
 from langchain.memory import ConversationBufferWindowMemory
-from langchain_community.tools.ddg_search.tool import DuckDuckGoSearchResults, DuckDuckGoSearchRun
+from langchain_community.tools.ddg_search.tool import DuckDuckGoSearchResults
+from langchain_community.utilities.arxiv import ArxivAPIWrapper
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
+from langchain.utilities import WikipediaAPIWrapper
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableMap
@@ -13,7 +17,6 @@ from langchain_core.tools import Tool
 from langsmith import Client
 
 from server.service.llama_functions import llm
-
 
 # Alot of this code is pulled from AgentToRouter.py and llama_functions.py
 def llama_complete(question: str, memory: ConversationBufferWindowMemory):
@@ -24,7 +27,7 @@ def llama_complete(question: str, memory: ConversationBufferWindowMemory):
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
     os.environ["LANGCHAIN_API_KEY"] = "ls__de882389727f48219891d9e0849bc26b"
-    os.environ["LANGCHAIN_PROJECT"] = "Llama2-70bchat-advanced"
+    os.environ["LANGCHAIN_PROJECT"] = "Llama2-refinedadvanced"
     client = Client()
 
     # Prompt template for conversation selection
@@ -33,12 +36,15 @@ def llama_complete(question: str, memory: ConversationBufferWindowMemory):
     single word, DO NOT add on any other statements, questions, or concerns.
     Choose one of the following actions:
     
-     - If the question is a greeting or has "you","your", "Learnix", or "I", references you as an AI, then respond ONLY
-      with 'GENERAL'
-     - If the question needs more information and you want to search the web and uses words such as "recent", "current", "now", "this year",
-      or time / day of the year, and is not in your training set, respond ONLY with 'SEARCH'
-     - If the question is about specific academic papers, meaning a DOI number (ex: 1888.083919) or specific academic author, respond ONLY
-      with 'PAPER'
+     - If the question is a greeting, a farewell, or contains personal pronouns like 'you', 'your', 'I', or names like 
+     'Learnix', or if it acknowledges you as an AI, then respond ONLY with 'GENERAL'. This includes any direct address 
+     or salutation, questions about personal experiences, or references to the AI's identity or capabilities.
+     - If a question suggests the need for the latest information or mentions time-sensitive words beyond 'recent',
+      'current', 'now', 'this year', like 'latest', 'updated', 'today', or any specific dates or times, and the answer
+       isn't in your knowledge base, respond with 'SEARCH' only. This applies to any terms that imply timeliness or that
+    the information may change frequently and is not historical or well-known.
+     - If the question is about specific academic papers, meaning a DOI number (ex: 1888.083919) or specific academic 
+     author, respond ONLY with 'PAPER'
      - If the question could be answered with some more information from academic papers, respond ONLY with 'PAPERSEARCH'
      - If you already have an answer or don't know which to respond to, respond ONLY with 'ANSWER'
      - If you don't know the answer and don't choose any previous options, respond ONLY with 'GENERAL'
@@ -53,7 +59,9 @@ def llama_complete(question: str, memory: ConversationBufferWindowMemory):
     # Base Chain Prompt
     base_chain = PromptTemplate.from_template("""
     You are called Learnix, but do not mention yourself, with the main goal to help people with their academics and research.
-    Keep the answer under 300 words max, but make sure to give a detailed answer.
+    Keep the answer under 300 words maximum. If the question is about a specific topic, provide a brief summary of the
+    topic. If the question is about a specific event, provide a brief summary of the event. If the question is about a
+    specific person, provide a brief summary of the person.
     The current year is""" + str(date.today().year) + """ and the current date is """ + str(date.today()) + """.
     If there was a previous conversation, here is the history: {chat_history}
     Respond to the question:
@@ -61,33 +69,36 @@ def llama_complete(question: str, memory: ConversationBufferWindowMemory):
 
     # Prompt for Searching
     # Can also sub PromptTemplate for hub.pull("zac-dot/react-adjusted")
-    react_prompt = PromptTemplate.from_template("""
-    You are Learnix, an academic librarian who helps people academically.
-    Answer questions the user gives as best as you can, giving as much information as possible when using the tools.
-    You can use the tools up to 2 times before summarizing and giving your final answer.
+    search_prompt = PromptTemplate.from_template("""
+    "The current year is """ + str(date.today().year) + """ and the current date is """ + str(date.today()) + """.You are
+    Learnix, an academic librarian. Your task is to assist users academically using a structured response format without
+    correcting structure or format of the question. Here's how you must structure your responses:
 
-    The current year is """ + str(date.today().year) + """ and the current date is """ + str(date.today()) + """.
+    1. Thought: Consider if using a tool is necessary. Answer 'Yes' or 'No'.
+    2. If 'Yes':
+    - Action: Specify which tool you will use, it must be the tools exact name from [{tool_names}], and should not have
+    any punctuation (Example: '.') or additional text.
+    - Action Input: Give the input you want to the tool, make sure to follow what the tool expects for Action Input
+    - Observation: Summarize the outcome of using the tool.
+    3. If 'No' or after using tools:
+    - Final Answer: Provide a comprehensive answer to the question.
+
+    It is crucial to follow this format strictly. For example:
+    
+    Thought: Do I need to use a tool? Yes
+    Action: Wikipedia
+    Action Input: 'Quantum Computing Basics'
+    Observation: Wikipedia provided a detailed overview of quantum computing principles.
+    Thought: Do I need to use a tool? No
+    Final Answer: Quantum computing is a field of computing focused on developing computer technology based on the
+    principles of quantum theory...
+
     You have access to the following tools:
 
     {tools}
-
-    To use a tool, please use the following format:
-
-    ```
-    Thought: Do I need to use a tool? Yes
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ```
     
-    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-    
-    ```
-    Thought: Do I need to use a tool? No
-    Final Answer: [your response here]
-    ```
-
-    Begin!
+    Do not include any additional text outside of Thought, Action, Action Input, Observation, or Final Answer.
+    Your response must strictly adhere to the provided structure. Begin!
     
     Question: {input}
     Thought:{agent_scratchpad}
@@ -96,61 +107,88 @@ def llama_complete(question: str, memory: ConversationBufferWindowMemory):
     # Prompt for Paper Searching
     # Can also sub out PromptTemplate for hub.pull("zac-dot/react-adjusted-papersearch")
     papersearch_prompt = PromptTemplate.from_template("""
-    You are Learnix, an academic librarian who helps people academically.
-    Answer the following questions as best you can.
-    The current year is """ + str(date.today().year) + """ and the current date is """ + str(date.today()) + """.
+    "The current year is """ + str(date.today().year) + """ and the current date is """ + str(date.today()) + """.You are
+    Learnix, an academic librarian. Your task is to assist users academically using a structured response format without
+    correcting structure or format of the question. Here's how you must structure your responses:
+
+    1. Thought: Consider if using a tool is necessary. Answer 'Yes' or 'No'.
+    2. If 'Yes':
+    - Action: Specify which tool you will use, it must be the tools exact name from [{tool_names}], and should not have
+    any punctuation (Example: '.') or additional text.
+    - Action Input: Give the input you want to the tool, make sure to follow what the tool expects for Action Input
+    - Observation: Summarize the outcome of using the tool.
+    3. If 'No' or after using tools:
+    - Final Answer: Provide a comprehensive answer to the question.
+
+    It is crucial to follow this format strictly. For example:
     
-    To use a tool, please use the following format:
+    Thought: Do I need to use a tool? Yes
+    Action: Wikipedia
+    Action Input: 'Quantum Computing Basics'
+    Observation: Wikipedia provided a detailed overview of quantum computing principles.
+    Thought: Do I need to use a tool? No
+    Final Answer: Quantum computing is a field of computing focused on developing computer technology based on the
+    principles of quantum theory...
+
+    You have access to the following tools:
 
     {tools}
     
-    ```
-    Thought: Do I need to use a tool? Yes
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ```
-    
-    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-    
-    ```
-    Thought: Do I need to use a tool? No
-    Final Answer: [your response here]
-    ```
-
-    Begin!
+    Do not include any additional text outside of Thought, Action, Action Input, Observation, or Final Answer.
+    Your response must strictly adhere to the provided structure. Begin!
     
     Question: {input}
     Thought:{agent_scratchpad}
     """)
 
     # DDG search tool
-    search_wrapper = DuckDuckGoSearchAPIWrapper(max_results=3)
-    search = DuckDuckGoSearchResults(api_wrapper=search_wrapper)
+    search = DuckDuckGoSearchResults(api_wrapper=DuckDuckGoSearchAPIWrapper(max_results=3))
     search_tool = [
         Tool(
             name="DuckDuckGo",
             func=search.run,
-            description="A search engine. Used to search the internet for current and up-to-date information."
-                        "Action Input should be a search query. Output is a JSON array of the query results."
-                        "Each time using this tool, the Action Input should be a different search query."
+            description="""When parsing information from DuckDuckGo, especially for queries for
+            example: 'Super Bowl winners,' it's important to focus on extracting factual data from these Instant Answers
+            or the list of search results provided. Action Input must be the exact search query for the paper. Example:
+            'Quantum Computing' """
         ),
     ]
 
     # Arxiv search tool
-    arxivsearch = load_tools(["arxiv"])
-    # TBD: semantic scholar
-    # semanticsearch = [SemanticScholarQueryRun()]
+    arxivsearch = ArxivAPIWrapper()
+    wikipedia = WikipediaAPIWrapper()
+    paper_tools = [
+        Tool(
+            name="Arxiv",
+            func=arxivsearch.run,
+            description="""Searches for the 3 most recent papers on the topic. In this case, it is used to search for
+            academic papers on the web. Information from this tool is always accurate, but make sure to summarize the
+            information. When using the information from the tool, make sure to tell the user information from arxiv 
+            only shows the 3 most recent papers from the author, on the topic, or similar to DOI number that is given.
+            Action Input must be the exact search query for the paper. Example: 'Quantum Computing'
+            """
+        ),
+        Tool(
+            name="Wikipedia",
+            func=wikipedia.run,
+            description="""Used to search for information in a more granular way, meaning alot more details on general
+            topics. This can be used for authors, places, events, topics, and more. When using the information from the 
+            tool, make sure to tell the user that the information from wikipedia is often accurate but should be
+            verified alongside other sources.
+            Action Input must be the exact search query for the topic. Example: 'Evolution of Quantum Computing' 
+            """
+        )
+    ]
 
     # Agent creation
-    search_agent = create_react_agent(llm=llama, tools=search_tool, prompt=react_prompt)
-    research_agent = create_react_agent(llm=llama, tools=arxivsearch, prompt=papersearch_prompt)
+    search_agent = create_react_agent(llm=llama, tools=search_tool, prompt=search_prompt)
+    research_agent = create_react_agent(llm=llama, tools=paper_tools, prompt=papersearch_prompt)
 
     # Executors for each agent
     search_executor = AgentExecutor(agent=search_agent, tools=search_tool, verbose=True, return_intermediate_steps=True,
                                     max_iterations=5, handle_parsing_errors=True)
-    research_executor = AgentExecutor(agent=research_agent, tools=arxivsearch, verbose=True,
-                                      return_intermediate_steps=True, max_iterations=3, handle_parsing_errors=True)
+    research_executor = AgentExecutor(agent=research_agent, tools=paper_tools, verbose=True,
+                                      return_intermediate_steps=True, max_iterations=5, handle_parsing_errors=True)
 
     # Defining the routing chain
     router_chain = prompt | llama | StrOutputParser()
