0086edf7c6b4fac91a67896f0fd351e345aa4fe5
Zac-dot <xanderthomp@gmail.com>
2024-03-03 17:48:21 -0800

    Flask on server is now running automatically. LlamaServerTesting.py works to test responses

--- aiscratch/LlamaServerTesting.py
+++ aiscratch/LlamaServerTesting.py
@@ -2,13 +2,13 @@ import requests
 # /api/llama/llama_complete
 # Server ip is: 52.13.109.29, using locally use localhost:5000
 userid = 1
-url = f"http://localhost:5000/api/llama/llama_complete"
+url = f"http://52.13.109.29/api/llama/llama_complete"
 data = {
     "question": "What is the meaning of life?",
     "userid": userid
 }
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=data, timeout=200)
 if response.status_code == 200:
     print(f"Success:", response.json())
 else:
